17b70c791ed47b85ee55d23e95e9a6a94cc45094
MarcioPezzi <marciopezzi92@gmail.com>
2023-05-11 23:37:40 -0300

    feat: comment box component

---
+++ src/components/Comment.jsx
@@ -0,0 +1,31 @@
+import styles from './Comment.module.css';
+import {Trash, ThumbsUp} from 'phosphor-react';
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/MarcioPezzi92.png" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong></strong>
+              <time title='11 de Maio às 08:13h' dateTime='2022-05-11 08:13:30'>Cerca de 1h atrás</time>
+            </div>
+            <button title='Deletar comentário'>
+              <Trash size={24} />
+            </button>
+          </header>
+          <p>Muito bom, Jubileu, parabéns!!</p>
+        </div>
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
